944d542cfce1297b4ee621ae2b4e74d4f563c65a
surajpatel121999 <surajpatel121999@gmail.com>
2024-07-23 20:10:59 +0530

    first version

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart'; // Ensure this import is correct
+import LoginSignup from './Pages/loginSignup'; // Ensure this import is correct
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="mens" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+          {/* Consider adding a catch-all route for 404 pages here */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
